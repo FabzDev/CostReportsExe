044e2c2d99b3b384059fc45128779d689f3df850
Fabio <fabioescobarardila@hotmail.com>
2024-01-18 00:44:32 -0500

    sarnovaCostreportsV1: script to run Sarnova reports

--- Script/sarnovaCostReportsV1.py
+++ Script/sarnovaCostReportsV1.py
@@ -7,7 +7,9 @@ import os
 
 def get_files():
     global prev_primary_col, new_primary_col, prev_file, new_file
-    os.chdir('../SarnovaReports')
+    os.chdir('d:/CostReports/Automatization')
+    # pd.set_option('display.max_columns', None)
+
     reportes = sorted(os.listdir())
     if len(os.path.basename(reportes[0])) > len(os.path.basename(reportes[1])):
         prev_file = reportes[0]
@@ -22,14 +24,24 @@ def get_files():
 def get_carriers():
     global prev_carrier_name, new_carrier_name
     prev_carrier_name = prev_primary_col[0].split(' ')[0]
-    new_carrier_name= new_primary_col[0].split(' ')[0];  
+    new_carrier_name= new_primary_col[0].split(' ')[0]; 
 
 
 def get_clients():
     global prev_client_name, new_client_name
-    prev_client_name = prev_primary_col[4]
-    new_client_name = new_primary_col[4]
+    prev_client_name = client_name_formatted(pd.read_excel(prev_file, sheet_name='AP Detail')['Unnamed: 1'][2])
+    new_client_name = client_name_formatted(pd.read_excel(new_file, sheet_name='AP Detail')['Unnamed: 1'][2])
 
+def client_name_formatted(client_name):
+    dict_name = {
+        'DIGITECH' : 'All OTHER DIVISIONS',
+        'BOUNDTREE MEDICAL' : 'Boundtree',
+        'CARDIO PARTNERS' : 'Cardio',
+        'EMERGENCY MEDICAL PRODUCTS' :'EMP',
+        'TRI-ANIM HEALTH SERVICES' : 'Tri Anim'
+    }
+    return dict_name.get(client_name, 'ERROR_IN_CLIENT_NAME')
+    
 
 def get_amounts():
     global prev_total_amount, new_total_amount
@@ -40,8 +52,8 @@ def get_amounts():
 
 def get_late_payment():
     global late_payment_amount
-    late_payment_col = pd.read_excel(new_file, sheet_name='Summary')
-    late_payment_amount =  late_payment_col[new_client_name][35]
+    late_payment_amount = pd.read_excel(new_file, sheet_name='Summary').iloc[35, 4]
+    # late_payment_amount = pd.read_excel(new_file, sheet_name='Summary').iloc[:, 4][35]
 
 
 def get_dates():
@@ -77,23 +89,39 @@ def check_dupes():
     there_no_dupes = vlookup[vlookup['lookup_column_y'] == 1].empty;
 
 
+def check_glcode():
+    global new_glcode_df
+    new_glcode_df_raw= pd.read_excel(new_file, sheet_name='AP Detail')[['Unnamed: 3', 'Unnamed: 6']]
+    new_glcode_df = new_glcode_df_raw.loc[
+        (new_glcode_df_raw['Unnamed: 3'].notna()) & 
+        (new_glcode_df_raw['Unnamed: 3'] != 'INVOICE NUMBER')
+        ]
+    new_glcode_df = new_glcode_df.loc[new_glcode_df_raw['Unnamed: 3'].isna()]
+
+
 def final_validation():
-    global client_matches, carrier_matches, amount_valid, late_payment_amount_valid
+    global client_matches, carrier_matches, amount_valid, late_payment_amount_valid, glcodes_valid
     client_matches = new_client_name == prev_client_name
     carrier_matches = new_carrier_name == prev_carrier_name
     # are_dates_correct
     amount_valid = (min(new_total_amount,prev_total_amount) / max(new_total_amount,prev_total_amount) *100) > 35
     late_payment_amount_valid = late_payment_amount == 0
     # are_there_dupes
-    return client_matches and carrier_matches and are_dates_correct and amount_valid and late_payment_amount_valid and there_no_dupes
+    glcodes_valid = new_glcode_df.empty
+    return (client_matches and 
+            carrier_matches and 
+            are_dates_correct and 
+            amount_valid and 
+            late_payment_amount_valid and 
+            there_no_dupes and 
+            glcodes_valid)
 
 
 def change_file_name(finalValidation: bool):
     if finalValidation:
-        os.rename(new_file, f'{new_client_name.capitalize()} {new_carrier_name.upper()} Parcel Cost Report WE{date_formatted}.xlsx')
+        os.rename(new_file, f'Sarnova {new_carrier_name.upper()} Parcel Cost Report {new_client_name} WE{date_formatted}.xlsx')
     else:
-        os.rename(new_file, f'REVIEW - {new_client_name.capitalize()} {new_carrier_name.upper()} Parcel Cost Report WE{date_formatted}.xlsx')
-
+        os.rename(new_file, f'REVIEW - {new_carrier_name.upper()} Parcel Cost Report {new_client_name} WE{date_formatted}.xlsx')
 
 
 def main():
@@ -104,7 +132,9 @@ def main():
     get_late_payment()
     get_dates()
     check_dupes()
+    check_glcode()
     change_file_name(final_validation())
+
 
 
 if __name__ == "__main__":
@@ -115,4 +145,11 @@ if __name__ == "__main__":
     else:
         input('\t❌WARNING: THE PROCESS ENCOUNTERED AN ERROR. PLEASE CHECK VALIDATIONS!! ')
     
-    input(f'\nClient matches: {client_matches}\nCarrier matches: {carrier_matches}\nDate matches: {are_dates_correct}\nTotal Amounts validated: {amount_valid}\nLate Payment fee $0: {late_payment_amount_valid}\nNo duplicates: {there_no_dupes}' )
+    input (f'''
+           Client matches: {client_matches}
+           Carrier matches: {carrier_matches}
+           Date matches: {are_dates_correct}
+           Total Amounts validated: {amount_valid}
+           Late Payment fee $0: {late_payment_amount_valid}
+           GL Accounts valid: {glcodes_valid}
+           No duplicates: {there_no_dupes}''')
